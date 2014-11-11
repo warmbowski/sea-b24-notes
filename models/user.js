3b9e49036f62fd87e1e5c023344852e0e6e4df48
Paul Laskowski <pelaskowski@gmail.com>
2014-11-10 16:03:54 -0800

    added auth/auth functionality

---
+++ models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jwt-simple');
+
+var userSchema = mongoose.Schema({
+  basic: {
+    email: {type: String},
+    password: {type: String}
+  }
+});
+
+// function isEmail(v) {
+//   return v;
+// }
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.basic.password);
+};
+
+userSchema.methods.generateToken = function(secret) {
+  var self = this;
+  var token = jwt.encode({
+    iss: self._id;
+  }, secret);
+  return token;
+};
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
+var passport = require('passport');
+var
 var app = express();
 
-app.use(bodyparser.json());
-
 mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGO_URL || 'mongodb://localhost/notes_development');
+app.use(bodyparser.json());
+app.set('jwtSecret', process.env.JWT_SECRET || 'changethisordie');
+app.set('secret', process.env.SECRET || 'changethistoo');
 
+app.use(passport.initialize());
+
+require('./lib/passport')(passport);
+
+require('./routes/users_routes')(app, passport);
 require('./routes/notes_routes')(app);
 
 app.set('port', process.env.PORT || 3000);
